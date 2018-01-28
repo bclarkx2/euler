aaebfb54d09f19fb36c378eca9b7783266d68964
Brian Clark <bclarkx2@gmail.com>
2018-01-28 13:09:08 -0500

    Add python euler framework class

--- solutions/common.py
+++ solutions/common.py
@@ -1,4 +1,51 @@
 
+###############################################################################
+# Imports                                                                     #
+###############################################################################
+
+import argparse
+
+
+###############################################################################
+# Classes                                                                     #
+###############################################################################
+
+class Euler(object):
+    """base class to solve an Euler problem"""
+    def __init__(self, arg_name, default_arg):
+        super(Euler, self).__init__()
+        self.arg_name = arg_name
+        self.default_arg = default_arg
+
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument("--" + arg_name,
+                                 type=int,
+                                 default=default_arg)
+        self.args = self.parse_args()
+        self.pull_main_arg()
+
+    def add_argument(self, *args):
+        return self.parser.add_argument(*args)
+
+    def parse_args(self):
+        return self.parser.parse_args()
+
+    def pull_main_arg(self):
+        setattr(self,
+                self.arg_name,
+                getattr(self.args, self.arg_name))
+
+    def solve(self):
+        raise NotImplementedError("provide solve method")
+
+    def out(self):
+        print(self.solve())
+
+
+###############################################################################
+# Utility                                                                     #
+###############################################################################
+
 def divisible(a, b):
     return a % b == 0
 
